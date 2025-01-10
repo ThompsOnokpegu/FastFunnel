b62dddb83c037b6d9597d834a36b18aeb589c520
AJThompson <tommyriode@gmail.com>
2025-01-10 12:07:32 +0100

    Webhook Fixed

--- resources/views/livewire/fast-funnel.blade.php
+++ resources/views/livewire/fast-funnel.blade.php
@@ -3,7 +3,7 @@
         <div class="mr-auto lg:col-span-6">
             <img class=" rounded-xl" src="{{ asset('img/fastfunnel.png') }}" alt="Fast Funnel">
             {{-- <p class="mb-3 text-lg italic text-black lg:text-xl">Stop losing money on ads that don’t work?</p> --}}
-            <h1 class="max-w-2xl my-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Get Paying Clients in Just 24 Hours with ₦8000 Ad</h1>
+            <h1 class="max-w-2xl my-4 pl-2 text-5xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Get Paying Clients in Just 24 Hours with ₦8000 Ad</h1>
             {{-- <p class="max-w-2xl mb-6 font-light text-black lg:mb-8 md:text-lg lg:text-xl">Follow my proven 3-step system to attract leads and close sales.</p> --}}
             <div class="ml-10">
                 <ul class="max-w-xl mb-4 space-y-4 text-lg sm:text-2xl text-black list-disc list-outside">
@@ -23,10 +23,11 @@
             </div>      
         </div>
         <div class="mt-5 sm:mt-0 lg:col-span-5 border rounded-md border-gray-100 shadow-lg">
-            
-            <form wire:submit="payOnce" class="w-full bg-white p-4 sm:p-6 lg:max-w-xl lg:p-8"> 
+            <p class="mt-1.5"><span class="text-base p-2 rounded-b-xl font-semibold bg-black text-deepr_green-50">Today's Price</span></p>
+            <form wire:submit="payOnce" wire:loading.class="opacity-40" class="w-full bg-white p-4 sm:p-6 lg:max-w-xl lg:p-8">
+                 
                 <h2 class="text-3xl font-semibold text-gray-900">₦13000 NGN</h2>
-                <span class="text-base p-1 font-semibold bg-black text-deepr_green-50">Today's Price</span>       
+                       
                 <section class="bg-white py-3 antialiased">
                     
                     <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
@@ -36,7 +37,7 @@
                                 <div class="mb-6 grid grid-cols-2 gap-4">
                                     <div class="col-span-2">
                                         <label for="full_name" class="mb-2 block text-sm font-medium text-gray-900"> Full name* </label>
-                                        <input type="text" id="full_name" wire:model="billing_name" class="block w-full border rounded border-gray-300 bg-gray-50 py-4 px-5 text-sm text-gray-900 focus:border-green-500 focus:ring-green-500" placeholder="AJ Thompson"/>
+                                        <input type="text" id="full_name" wire:model="billing_name" class="block w-full border rounded border-gray-300 bg-white py-4 px-5 text-md text-gray-900 focus:border-green-500 focus:ring-green-500" placeholder="AJ Thompson"/>
                                         <div>
                                             @error('billing_name') <span class="error text-deepr_red-50">{{ $message }}</span> @enderror 
                                         </div>
@@ -44,18 +45,18 @@
                         
                                     <div class="col-span-2">
                                         <label for="email" class="mb-2 block text-sm font-medium text-gray-900"> Email address* </label>
-                                        <input type="email" id="email" wire:model="billing_email" class="block w-full border rounded border-gray-300 bg-gray-50 py-4 px-5 pe-10 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500" placeholder="Email Address"/>
+                                        <input type="email" id="email" wire:model="billing_email" class="block w-full border rounded border-gray-300 bg-white py-4 px-5 pe-10 text-md text-gray-900 focus:border-green-500 focus:ring-green-500" placeholder="Email Address"/>
                                         <div>
                                             @error('billing_email') <span class="error text-deepr_red-50">{{ $message }}</span> @enderror 
                                         </div>
                                     </div> 
                                 </div>
                                 <div class="mt-6 grow sm:mt-8 lg:mt-8">
-                                    <div class="space-y-4 rounded-lg border border-gray-100 bg-gray-50 p-6">
+                                    <div class="space-y-4 rounded-lg border border-gray-100 bg-green-50 p-6">
                                         <div class="space-y-2">
                                             <dl class="flex items-center justify-between gap-4">
                                             <dt class="text-base font-normal text-gray-500 ">Original price</dt>
-                                            <dd class="text-base font-medium text-gray-900">₦32000</dd>
+                                            <dd class="text-base line-through font-medium text-gray-900">₦32000</dd>
                                             </dl>
                             
                                             <dl class="flex items-center justify-between gap-4">
